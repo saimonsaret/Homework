5745d77b575d6de5031268721ca51e0449d7d180
Semen Yuriev <saimon_saret@mail.ru>
2014-05-05 12:20:22 +0400

    Исправлена задача 7.2
    Удаляется помять под координаты; методы вынесены за пределы шаблонного
    класса

--- 1 course, 2 semester/7/2/vector.h
+++ 1 course, 2 semester/7/2/vector.h
@@ -3,52 +3,79 @@
 template<int dimension>
 class Vector {
 	public:
-		Vector() {
-			coordinates = new double[dimension];
-			for (int i = 0; i < dimension; i++)
-				coordinates[i] = 0;
-		}
-		Vector(const double input[dimension]) {
-			coordinates = new double[dimension];
-			for (int i = 0; i < dimension; i++)
-				coordinates[i] = input[i];
-		}
-		Vector(Vector<dimension> const &vect) {
-			coordinates = new double[dimension];
-			for (int i = 0; i < dimension; i++)
-				coordinates[i] = vect.coordinates[i];
-		}
-		Vector<dimension> &operator+(Vector<dimension> const &vect) {
-			Vector<dimension> *newVect = new Vector<dimension>;
-			for (int i = 0; i < dimension; i++)
-				newVect->coordinates[i] = this->coordinates[i] + vect.coordinates[i];
-			return *newVect;
-		}
-		Vector<dimension> &operator-(Vector<dimension> const &vect) {
-			Vector<dimension> *newVect = new Vector<dimension>;
-			for (int i = 0; i < dimension; i++)
-				newVect->coordinates[i] = this->coordinates[i] - vect.coordinates[i];
-			return *newVect;
-		}
-		bool operator==(Vector<dimension> const &vect) const {
-			for (int i = 0; i < dimension; i++)
-				if (this->coordinates[i] != vect.coordinates[i])
-					return false;
-			return true;
-		}
-		static double scalarMult(Vector<dimension> const &first, Vector<dimension> const &second) {
-			double res = 0;
-			for (int i = 0; i < dimension; i++)
-				res += first.coordinates[i] * second.coordinates[i];
-			return res;
-		}
+		Vector();
+		Vector(const double input[dimension]);
+		Vector(Vector<dimension> const &vect);
+		Vector<dimension> &operator+(Vector<dimension> const &vect);
+		Vector<dimension> &operator-(Vector<dimension> const &vect);
+		bool operator==(Vector<dimension> const &vect) const;
+
+		static double scalarMult(Vector<dimension> const &first, Vector<dimension> const &second);
 		bool isNull() {
 			Vector<dimension> nullVect;
 			return (*this == nullVect);
 		}
 		~Vector() {
+			delete coordinates;
 		}
 
 	private:
 		double *coordinates;
 };
+
+
+
+template<int dimension>
+Vector<dimension>::Vector() {
+	coordinates = new double[dimension];
+	for (int i = 0; i < dimension; i++)
+		coordinates[i] = 0;
+}
+
+///Coordinates constructor
+template<int dimension>
+Vector<dimension>::Vector(const double input[dimension]) {
+	coordinates = new double[dimension];
+	for (int i = 0; i < dimension; i++)
+		coordinates[i] = input[i];
+}
+
+///Copy constructor
+template<int dimension>
+Vector<dimension>::Vector(Vector<dimension> const &vect) {
+	coordinates = new double[dimension];
+	for (int i = 0; i < dimension; i++)
+		coordinates[i] = vect.coordinates[i];
+}
+
+template<int dimension>
+Vector<dimension>& Vector<dimension>::operator+(Vector<dimension> const &vect) {
+	Vector<dimension> *newVect = new Vector<dimension>;
+	for (int i = 0; i < dimension; i++)
+		newVect->coordinates[i] = this->coordinates[i] + vect.coordinates[i];
+	return *newVect;
+}
+
+template<int dimension>
+Vector<dimension>& Vector<dimension>::operator-(Vector<dimension> const &vect) {
+	Vector<dimension> *newVect = new Vector<dimension>;
+	for (int i = 0; i < dimension; i++)
+		newVect->coordinates[i] = this->coordinates[i] - vect.coordinates[i];
+	return *newVect;
+}
+
+template<int dimension>
+bool Vector<dimension>::operator==(Vector<dimension> const &vect) const {
+	for (int i = 0; i < dimension; i++)
+		if (this->coordinates[i] != vect.coordinates[i])
+			return false;
+	return true;
+}
+
+template<int dimension>
+double Vector<dimension>::scalarMult(Vector<dimension> const &first, Vector<dimension> const &second) {
+	double res = 0;
+	for (int i = 0; i < dimension; i++)
+		res += first.coordinates[i] * second.coordinates[i];
+	return res;
+}
