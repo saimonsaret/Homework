55f9a885476c12977e230926e25ce0f414764fd2
Semen Yuriev <saimon_saret@mail.ru>
2015-02-23 20:04:30 +0400

    Another tabbing fix in homework 2

--- 2 course, 2 semester/2/4.hs
+++ 2 course, 2 semester/2/4.hs
@@ -3,6 +3,6 @@ find([], n) = -1
 find(list, n) = if head list == n
                 then 1
                 else let ans = find(tail list, n)
-			         in if ans == -1
-				        then -1
-					    else ans + 1
+                     in if ans == -1
+                        then -1
+                        else ans + 1
