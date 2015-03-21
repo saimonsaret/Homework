8ad82b85ee10ebced6deb7fb7280dfec4624ccf3
Semen Yuriev <saimon_saret@mail.ru>
2015-03-21 16:53:32 +0400

    Fix tabbing in 3.3

--- 2 course, 2 semester/3/3.hs
+++ 2 course, 2 semester/3/3.hs
@@ -1,11 +1,11 @@
 findMax :: ([Integer], Integer, Integer, Integer) -> Integer
 findMax (x1:x2:x3:list, curMax, pos, maxPos) = if list == []
                                                then if x1 + x3 > curMax
-								                    then pos
-											        else maxPos
-							                   else if x1 + x3 > curMax
+                                                    then pos
+                                                    else maxPos
+                                               else if x1 + x3 > curMax
                                                     then findMax(x2:x3:list, x1 + x3, pos + 1, pos)
-											        else findMax(x2:x3:list, curMax, pos + 1, maxPos)
+                                                    else findMax(x2:x3:list, curMax, pos + 1, maxPos)
 maxPos list = let len = length list
 			  in if (len <= 2)
 			  then -1
