2e97106c5640406eab3e320a519d899f9970b754
Semen Yuriev <saimon_saret@mail.ru>
2015-05-28 03:39:52 +0400

    Correct 7.4

--- 2 course, 2 semester/7/4.hs
+++ 2 course, 2 semester/7/4.hs
@@ -3,7 +3,7 @@ import Control.Monad.Cont
 newMap :: (a -> b) -> [a] -> Cont r [b]
 newMap func []  = return []
 newMap func (x:xs) = do
-                     ys <- newMap f xs
+                     ys <- newMap func xs
                      return ((func x):ys)
 
 newFilter :: (a -> Bool) -> [a] -> Cont r [a]
